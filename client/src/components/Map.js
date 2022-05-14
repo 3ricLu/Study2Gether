f3e50291977b40210c51cf4b89df52fa1d067909
3ricLu <EricLu.Canada@gmail.com>
2022-05-14 16:00:10 -0400

    working with markers

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -4,10 +4,42 @@ import Navigation from './Navigation';
 import {GoogleMap, withScriptjs, Marker, InfoWindow} from 'react-google-maps';
 
 function Map() {
+
+    // Hook for building selection 
+    const [selectedBuilding, setSelectedBuilding] = useState("");
+
     return (
-        <GoogleMap
-        defaultZoom={18}
-        defaultCenter={{ lat: 43.473559, lng: -80.527860}}/>
+        <div> 
+            <GoogleMap
+            defaultZoom={18}
+            defaultCenter={{ lat: 43.473559, lng: -80.527860}}/>
+
+            <Marker
+            position={{ lat: 43.475059, lng: -80.529466 }}
+            onClick={() => {
+                setSelectedBuilding("Lazaridis");
+                console.log("Laz clicked");
+            }}
+            />
+            
+            {/* MARKER */}
+            {selectedBuilding == "Lazaridis" && (
+                <InfoWindow
+                    position={{ lat: 43.475059, lng: -80.529466 }}>
+                    <div>Lazaridis</div>
+                </InfoWindow>
+            )}
+
+            {/* MARKER */}
+            {selectedBuilding == "Lazaridis" && (
+                <InfoWindow
+                    position={{ lat: 43.475059, lng: -80.529466 }}>
+                    <div>Lazaridis</div>
+                </InfoWindow>
+            )}
+            
+        </div>
+
         )
 }
 
