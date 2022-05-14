a57ec24b17f36b455e31d717e1779016f19413c1
3ricLu <EricLu.Canada@gmail.com>
2022-05-14 13:59:13 -0400

    stuff

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -6,8 +6,8 @@ import {GoogleMap, withScriptjs, Marker, InfoWindow} from 'react-google-maps';
 function Map() {
     return (
         <GoogleMap
-        defaultZoom={10}
-        defaultCenter={{ lat: 43.473217, lng: -80.527977}}/>
+        defaultZoom={18}
+        defaultCenter={{ lat: 43.473559, lng: -80.527860}}/>
         )
 }
 
